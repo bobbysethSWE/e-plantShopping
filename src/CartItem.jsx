201a421bbc3709c5753938d4ae52f67d1f0d7e4c
Arham <twinm148@gmail.com>
2025-03-02 10:25:37 -0500

    removed duplicate imports

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -2,11 +2,7 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
-import { useDispatch } from 'react-redux';
-import { updateQuantity } from './CartSlice';
 import { addItem } from './CartSlice';
-import { removeItem } from './CartSlice';
-'./CartSlice';
 
 const CartItem = ({ onContinueShopping }) => {
   const cart = useSelector(state => state.cart.items);
